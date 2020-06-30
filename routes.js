4f17a99f314704f4b1cc2d5e6433837234e3c08e
Victor <victor.fernandes01@hotmail.com>
2020-06-30 10:48:37 -0300

    New routes

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = express.Router();
+const instructors  = require('./instructors');
 
 routes.get('/', function (req, res) {
     return res.redirect("/instructors");
@@ -9,19 +10,12 @@ routes.get('/instructors', function (req, res) {
     return res.render("instructors/index");
 })
 
-routes.post("/instructors", function (req, res) {
-    const keys = Object.keys(req.body)
-    for(key of keys) {
-        if(req.body[key] == "") {
-            return res.send('Complete tudo');
-        }
-    }
-    return res.send(keys);
-});
-
 routes.get('/instructors/create', function (req, res) {
     return res.render("instructors/create");
 })
+
+routes.post("/instructors", instructors.post);
+
 
 routes.get('/members', function (req, res) {
     return res.send("members");
