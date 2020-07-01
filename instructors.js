a2b8ae4508b2a438a190239cb4ff0eb3d75256cc
Victor <victor.fernandes01@hotmail.com>
2020-07-01 09:21:35 -0300

    JSON

--- instructors.js
+++ instructors.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const data = require('./data.json');
+
 exports.post = function (req, res) {
     const keys = Object.keys(req.body)
     for(key of keys) {
@@ -5,5 +8,15 @@ exports.post = function (req, res) {
             return res.send('Complete tudo');
         }
     }
-    return res.send(keys);
+
+    data.instructors.push(req.body);
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function (){
+        if (err) {
+            return res.send("Write file error!");
+        }
+        return res.redirect("/nstructors")
+    });
+
+    //return res.send(keys);
 }
