532cdca250382fa0230fad16ad3b09fb22bc8244
Victor <victor.fernandes01@hotmail.com>
2020-07-02 11:20:08 -0300

    modulo04

---
+++ date.js
@@ -0,0 +1,14 @@
+function age(timestamp) {
+    const today = new Date();
+    const bithDate = new Date(timestamp);
+
+    let age = today.getFullYear() - bithDate.getFullYear();
+
+    const month = today.getMonth() - bithDate.getMonth();
+
+    if (month <= 0 && today.getDate() < birthDate.getDate()) {
+        age = age - 1;
+    }
+
+    return age;
+}
